0f33905405967fd33d216cf42aae481c44851f92
Terence Then <chenjienan2009@gmail.com>
2019-05-25 18:18:42 -0700

    May 25

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -3,8 +3,22 @@ from test_framework import generic_test
 
 def is_balanced_binary_tree(tree):
     # TODO - you fill in here.
-    return True
+    
+    def get_height(node):
+        if not node: return 0
 
+        # divide
+        left = get_height(node.left)
+        right = get_height(node.right)
+        
+        # conquer
+        if left == -1 or right == -1: return -1
+        if abs(left - right) > 1: return -1
+
+        return max(left, right) + 1
+
+    height = get_height(tree)
+    return height != -1
 
 if __name__ == '__main__':
     exit(
--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,9 +2,14 @@ from test_framework import generic_test
 
 
 def parity(x):
-    # TODO - you fill in here.
-    return 0
+    num_of_one = 0
+    s = bin(x)
+    for c in s[2:]:
+        if c == '1':
+            num_of_one += 1
+    return 1 if num_of_one % 2 != 0 else 0 
 
 
 if __name__ == '__main__':
     exit(generic_test.generic_test_main("parity.py", 'parity.tsv', parity))
+    # parity(55042960196257)
